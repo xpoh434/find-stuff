763cf054d22e66c096b7cff522556b9306826baa
tibo <tiboxpoh@gmail.com>
2016-03-23 01:53:59 +0800

    support zip

--- find_stuff/indexer.py
+++ find_stuff/indexer.py
@@ -4,27 +4,56 @@ Created on Mar 21, 2016
 '''
 
 from argparse import ArgumentParser
-from bs4 import BeautifulSoup
 from cStringIO import StringIO
 import codecs
-import djvu.decode
-import docx
-import epub
+from contextlib import contextmanager
+from find_stuff.common import load_config, CJKFilter
 from logging import getLogger, basicConfig
 import multiprocessing
 import os
-from os.path import exists
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
+from os.path import exists, join, splitext
+import shutil
+import tempfile
+from zipfile import ZipFile
+
 from whoosh.analysis.analyzers import StemmingAnalyzer
 from whoosh.fields import Schema, TEXT, ID, STORED
 from whoosh.index import create_in, open_dir
 from whoosh.util.text import rcompile
 
-from find_stuff.chmfile import SimpleChmFile
-from find_stuff.common import load_config, CJKFilter
+try:
+    from bs4 import BeautifulSoup
+except:
+    pass
+
+try:
+    import docx
+except:
+    pass
+
+try:
+    import epub
+except:
+    pass
+    
+try:
+    from pdfminer.converter import TextConverter
+    from pdfminer.layout import LAParams
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.pdfpage import PDFPage
+except:
+    pass
+
+try:
+    from find_stuff.chmfile import SimpleChmFile
+except:
+    pass
+
+try:
+    from pyth.plugins.rtf15.reader import Rtf15Reader
+    from pyth.plugins.plaintext.writer import PlaintextWriter
+except:
+    pass    
 
 
 logger = getLogger("indexer")
@@ -38,6 +67,8 @@ stem_ana2.cachesize = -1
 
 
 schema = Schema(title=TEXT(analyzer=stem_ana2,stored=True), content=TEXT(analyzer=stem_ana), time=STORED, path=ID(stored=True), filetype=ID)
+
+handlers = {}
 
 class TxtHandler(object):
     
@@ -67,9 +98,6 @@ def extract_html(html):
 class RtfHandler(object):
     
     def extract_content(self, filepath):
-        from pyth.plugins.rtf15.reader import Rtf15Reader
-        from pyth.plugins.plaintext.writer import PlaintextWriter
-        
         with open(filepath, "rb") as fh:
             doc = Rtf15Reader.read(fh)
         
@@ -116,39 +144,46 @@ class ChmHandler(object):
             sio.write("\n")
         return to_utf8(sio.getvalue())
         
-
-#http://apt-browse.org/browse/debian/wheezy/main/i386/python-djvu/0.3.9-1/file/usr/share/doc/python-djvu/examples/djvu-dump-text
-class DjvuHandler(object):
+try:
+    import djvu.decode
     
-    def get_text(self, sexpr):
-        sio = StringIO()
-        if isinstance(sexpr, djvu.sexpr.ListExpression):
-            #print str(sexpr[0].value), [sexpr[i].value for i in xrange(1, 5)]
-            for child in sexpr[5:]:
-                sio.write(self.get_text(child))
-                sio.write(" ")
-        else:
-            sio.write(sexpr.value.strip())
-            sio.write(" ")
-        return sio.getvalue()
-
-    class Context(djvu.decode.Context):
-        def handle_message(self, message):
-            if isinstance(message, djvu.decode.ErrorMessage):
-                logger.error(message)
-    
-    def extract_content(self, filepath):
-        ctx = self.Context()
-        document = ctx.new_document(djvu.decode.FileURI(filepath))
-        document.decoding_job.wait()
-        sio = StringIO()
-        for page in document.pages:
-#             page.get_info()
-            sio.write(self.get_text(page.text.sexpr))
-            sio.write("\n")
+    #http://apt-browse.org/browse/debian/wheezy/main/i386/python-djvu/0.3.9-1/file/usr/share/doc/python-djvu/examples/djvu-dump-text
+    class DjvuHandler(object):
         
-        txt = sio.getvalue()
-        return to_utf8(txt)
+        def get_text(self, sexpr):
+            sio = StringIO()
+            if isinstance(sexpr, djvu.sexpr.ListExpression):
+                #print str(sexpr[0].value), [sexpr[i].value for i in xrange(1, 5)]
+                for child in sexpr[5:]:
+                    sio.write(self.get_text(child))
+                    sio.write(" ")
+            else:
+                sio.write(sexpr.value.strip())
+                sio.write(" ")
+            return sio.getvalue()
+    
+        class Context(djvu.decode.Context):
+            def handle_message(self, message):
+                if isinstance(message, djvu.decode.ErrorMessage):
+                    logger.error(message)
+        
+        def extract_content(self, filepath):
+            ctx = self.Context()
+            document = ctx.new_document(djvu.decode.FileURI(filepath))
+            document.decoding_job.wait()
+            sio = StringIO()
+            for page in document.pages:
+    #             page.get_info()
+                sio.write(self.get_text(page.text.sexpr))
+                sio.write("\n")
+            
+            txt = sio.getvalue()
+            return to_utf8(txt)
+
+    handlers[".djvu"] = DjvuHandler()
+
+except:
+    pass
 
 
 class PdfHandler(object):
@@ -178,24 +213,101 @@ class PdfHandler(object):
                 device.close()
                 txt= unicode(outfp.getvalue(),encoding='utf-8')
                 outfp.close()
-        print len(txt)
-        print txt[:2000]
-        print txt[-2000:]
         return to_utf8(txt)
+    
+    
 
 htmlhdr = HtmlHandler()
 txthdr = TxtHandler()
-handlers = {
-            ".txt": txthdr,
-            ".pdf": PdfHandler(),
-            ".djvu": DjvuHandler(),
-            ".epub": EpubHandler(),
-            ".html": htmlhdr,
-            ".htm" : htmlhdr,
-            ".chm" : ChmHandler(),
-            ".docx": DocxHandler(),
-            ".rtf": RtfHandler()
-            }
+handlers[".txt"] = txthdr
+handlers[".pdf"] = PdfHandler()
+handlers[".epub"] = EpubHandler()
+handlers[".html"] = htmlhdr
+handlers[".htm"] = htmlhdr
+handlers[".chm"] = ChmHandler()
+handlers[".docx"] = DocxHandler()
+handlers[".rtf"] = RtfHandler()
+
+class ZipHandler(object):
+    
+    @contextmanager            
+    def extract(self, filepath):
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            with ZipFile(filepath,'r') as z:
+                tmppath = join(tmp_dir,os.path.basename(filepath))
+                os.makedirs(tmppath)
+                z.extractall(tmppath)
+                
+            yield tmppath
+        finally:
+            shutil.rmtree(tmp_dir)
+            
+    def path_exists(self, archive, path):
+        with ZipFile(archive,'r') as z:
+            return path in z.namelist()
+        
+
+archive_handlers = {
+                    ".zip": ZipHandler()
+                    }
+
+def walk_path(path):
+    for root,dirs_,files in os.walk(path):
+        archives = []
+        non_archives = []
+        for f in files:
+            _,ext = splitext(f)
+            if ext in archive_handlers:
+                archives.append(f)
+            else:
+                non_archives.append(f)
+        yield root, dirs_ + archives, non_archives, root
+        
+        for a in archives:
+            filepath=join(root, a)
+            _,ext = splitext(a)
+            
+            with archive_handlers[ext].extract(filepath) as realpath:
+                for root2, dirs2, files2 in os.walk(realpath):
+                    relpath=os.path.relpath(root2, realpath)
+                    yield join(filepath, relpath), dirs2, files2, root2
+
+def _get_archive_paths(path):
+    elems = path.split(os.path.sep)
+    for i,t in enumerate(elems):
+        _,ext = splitext(t)
+        if ext in archive_handlers:
+            break
+    else:
+        return None, None, None
+    archive_file = os.path.sep.join(elems[:i+1])
+    path_in_archive = "/".join(elems[i+1:])
+    
+    return ext, archive_file, path_in_archive
+
+def path_exists(path):
+    if not os.path.exists(path):
+        ext, archive_file, path_in_archive = _get_archive_paths(path)
+        if ext is None:
+            return False
+        else:
+            return os.path.exists(archive_file) and archive_handlers[ext].path_exists(archive_file, path_in_archive)
+    else:
+        return True
+    
+def getmtime(path):
+    if os.path.exists(path):
+        mtime = os.path.getmtime(path)
+    else:
+        ext, archive_file, _= _get_archive_paths(path)
+        if ext is None:
+            raise IOError, "path %s does not exists" % path
+        else:
+            mtime = os.path.getmtime(archive_file)
+        
+    return mtime
+        
 
 def get_handler(ext):
     return handlers.get(ext)
@@ -216,7 +328,8 @@ def incremental_index(ix, target_path, indexables, work_path):
             indexed_paths.add(indexed_path)
             
             filepath = os.path.join(target_path, indexed_path)
-            if not os.path.exists(filepath):
+            #if not os.path.exists(filepath):
+            if not path_exists(filepath):
                 # This file was deleted since it was indexed
                 logger.info("remove: %s", indexed_path)
                 writer.delete_by_term('path', indexed_path)
@@ -225,7 +338,7 @@ def incremental_index(ix, target_path, indexables, work_path):
                 # Check if this file was changed since it
                 # was indexed
                 indexed_time = fields['time']
-                mtime = os.path.getmtime(filepath)
+                mtime = getmtime(filepath)
                 if mtime > indexed_time:
                     # The file has changed, delete it and add it to the list of
                     # files to reindex
@@ -235,12 +348,12 @@ def incremental_index(ix, target_path, indexables, work_path):
         count = 0
         try:
             work_path = work_path or target_path
-            for root, _, files  in os.walk(work_path):
+            for root, _, files, realroot  in walk_path(work_path):
                 for f in files:
-                    filepath = os.path.join(root, f)
                     filename, ext = os.path.splitext(f)
                     if ext in indexables:
-                        relpath = os.path.relpath(filepath, target_path)
+                        virtualpath = os.path.join(root, f)
+                        relpath = os.path.relpath(virtualpath, target_path)
                         if relpath not in indexed_paths or relpath in to_index:
                             logger.info("indexing... %s", relpath)
                             hdr = get_handler(ext)
@@ -248,9 +361,10 @@ def incremental_index(ix, target_path, indexables, work_path):
                                 content = "" 
                             else:
                                 try:
+                                    filepath = os.path.join(realroot, f)
                                     content = hdr.extract_content(filepath.encode('utf-8'))
                                     writer.add_document(title=filename, content=content,
-                                                path=relpath, filetype=ext, time=os.path.getmtime(filepath))
+                                                path=relpath, filetype=ext, time=getmtime(virtualpath))
                                     count+=1
                                 except KeyboardInterrupt:
                                     raise

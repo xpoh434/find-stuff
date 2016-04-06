0afb62b7c3de96024ac669cee7c1378621c963d2
tibo <tiboxpoh@gmail.com>
2016-04-07 00:10:33 +0800

    fix archive, path

--- find_stuff/indexer.py
+++ find_stuff/indexer.py
@@ -8,35 +8,45 @@ from cStringIO import StringIO
 import codecs
 from contextlib import contextmanager
 from find_stuff.common import load_config, CJKFilter
+import gzip
 from logging import getLogger, basicConfig
 import multiprocessing
 import os
 from os.path import exists, join, splitext
 import shutil
+import tarfile
 import tempfile
 from zipfile import ZipFile
 
-import patoolib
+from rarfile import RarFile
+import rarfile
+
+if os.name == 'nt':
+    rarfile.UNRAR_TOOL = 'C:\\Program Files\\WinRAR\\unrar.exe'
+
 from whoosh.analysis.analyzers import StemmingAnalyzer
 from whoosh.fields import Schema, TEXT, ID, STORED
 from whoosh.index import create_in, open_dir
 from whoosh.util.text import rcompile
 
 
+logger = getLogger("indexer")
+basicConfig(level="INFO")
+
 try:
     from bs4 import BeautifulSoup
 except:
-    pass
+    logger.warn("failed to import BeautifulSoup")
 
 try:
     import docx
 except:
-    pass
+    logger.warn("failed to import docx")
 
 try:
     import epub
 except:
-    pass
+    logger.warn("failed to import epub")
     
 try:
     from pdfminer.converter import TextConverter
@@ -44,21 +54,19 @@ try:
     from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
     from pdfminer.pdfpage import PDFPage
 except:
-    pass
+    logger.warn("failed to import pdfminer")
 
 try:
     from find_stuff.chmfile import SimpleChmFile
 except:
-    pass
+    logger.warn("failed to import chm packages")    
 
 try:
     from pyth.plugins.rtf15.reader import Rtf15Reader
     from pyth.plugins.plaintext.writer import PlaintextWriter
 except:
-    pass    
+    logger.warn("failed to import rtf packages")    
 
-
-logger = getLogger("indexer")
 
 stem_ana = StemmingAnalyzer() | CJKFilter()
 stem_ana.cachesize = -1
@@ -68,7 +76,7 @@ stem_ana2 = StemmingAnalyzer(expression=pattern2) | CJKFilter()
 stem_ana2.cachesize = -1
 
 
-schema = Schema(title=TEXT(analyzer=stem_ana2,stored=True), content=TEXT(analyzer=stem_ana), time=STORED, path=ID(stored=True), filetype=ID)
+schema = Schema(title=TEXT(analyzer=stem_ana2,stored=True), content=TEXT(analyzer=stem_ana), time=STORED, path=ID(stored=True), real_path=STORED, filetype=ID)
 
 handlers = {}
 
@@ -185,7 +193,7 @@ try:
     handlers[".djvu"] = DjvuHandler()
 
 except:
-    pass
+    logger.warn("failed to initialize djvu handler")
 
 
 class PdfHandler(object):
@@ -230,16 +238,16 @@ handlers[".chm"] = ChmHandler()
 handlers[".docx"] = DocxHandler()
 handlers[".rtf"] = RtfHandler()
 
-class ArchiveHandler(object):
+class ZipHandler(object):
     
     @contextmanager            
     def extract(self, filepath):
         try:
             tmp_dir = tempfile.mkdtemp()
-            #with ZipFile(filepath,'r') as z:
-            tmppath = join(tmp_dir,os.path.basename(filepath))
-            os.makedirs(tmppath)
-            patoolib.extract_archive(filepath,outdir=tmppath)
+            with ZipFile(filepath,'r') as z:
+                tmppath = join(tmp_dir,os.path.basename(filepath))
+                os.makedirs(tmppath)
+                z.extractall(tmppath)
                 
             yield tmppath
         finally:
@@ -248,76 +256,133 @@ class ArchiveHandler(object):
     def path_exists(self, archive, path):
         with ZipFile(archive,'r') as z:
             return path in z.namelist()
-        
 
-generic_archive_handler = ArchiveHandler()
+class RarHandler(object):
+    
+    @contextmanager            
+    def extract(self, filepath):
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            with RarFile(filepath,'r') as z:
+                tmppath = join(tmp_dir,os.path.basename(filepath))
+                os.makedirs(tmppath)
+                z.extractall(tmppath)
+                
+            yield tmppath
+        finally:
+            shutil.rmtree(tmp_dir)
+            
+    def path_exists(self, archive, path):
+        with RarFile(archive,'r') as z:
+            return path in z.namelist()
+        
+class TarHandler(object):
+    
+    @contextmanager            
+    def extract(self, filepath):
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            with tarfile.open(filepath,'r') as z:
+                tmppath = join(tmp_dir,os.path.basename(filepath))
+                os.makedirs(tmppath)
+                z.extractall(tmppath)
+                
+            yield tmppath
+        finally:
+            shutil.rmtree(tmp_dir)
+            
+    def path_exists(self, archive, path):
+        with tarfile.open(archive,'r') as z:
+            return path in z.getnames()
+        
+class GzipHandler(object):
+    
+    @contextmanager            
+    def extract(self, filepath):
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            with gzip.open(filepath,'rb') as z:
+                tmppath = join(tmp_dir,os.path.basename(filepath))
+                os.makedirs(tmppath)
+                #z.extractall(tmppath)
+                bn, _ = os.path.splitext(os.path.basename(filepath))
+                with open(join(tmppath,bn),'wb') as f:
+                    f.write(z.read())
+                
+            yield tmppath
+        finally:
+            shutil.rmtree(tmp_dir)
+            
+    def path_exists(self, archive, path):
+        return path + ".gz" == archive
+        
 
 archive_handlers = {
-                    ".zip": generic_archive_handler,
-                    ".tar": generic_archive_handler,
-                    ".rar": generic_archive_handler,
-                    ".gz": generic_archive_handler
+                    ".zip": ZipHandler(),
+                    ".tar": TarHandler(),
+                    ".tar.gz": TarHandler(),
+                    ".rar": RarHandler(),
+                    ".gz": GzipHandler(),
                     }
 
-def walk_path(path):
-    for root,dirs_,files in os.walk(path):
-        archives = []
-        non_archives = []
-        for f in files:
-            _,ext = splitext(f)
-            if ext in archive_handlers:
-                archives.append(f)
-            else:
-                non_archives.append(f)
-        yield root, dirs_ + archives, non_archives, root
-        
-        for a in archives:
-            filepath=join(root, a)
-            _,ext = splitext(a)
-            
-            with archive_handlers[ext].extract(filepath) as realpath:
-                for root2, dirs2, files2 in os.walk(realpath):
-                    relpath=os.path.relpath(root2, realpath)
-                    yield join(filepath, relpath), dirs2, files2, root2
-
-def _get_archive_paths(path):
-    elems = path.split(os.path.sep)
-    for i,t in enumerate(elems):
-        _,ext = splitext(t)
-        if ext in archive_handlers:
-            break
+def os_path(p):
+    if os.name == 'nt':
+        return p.replace('/',os.path.sep)
     else:
-        return None, None, None
-    archive_file = os.path.sep.join(elems[:i+1])
-    path_in_archive = "/".join(elems[i+1:])
+        return p
+
+def std_path(p):
+    if os.name == 'nt':
+        return p.replace(os.path.sep, '/')
+    return p    
+
+def path_join(path, *args):
+    return os.path.join(path, *args)
     
-    return ext, archive_file, path_in_archive
-
+    
 def path_exists(path):
-    if not os.path.exists(path):
-        ext, archive_file, path_in_archive = _get_archive_paths(path)
-        if ext is None:
-            return False
-        else:
-            return os.path.exists(archive_file) and archive_handlers[ext].path_exists(archive_file, path_in_archive)
-    else:
-        return True
+    return os.path.exists(path)
     
 def getmtime(path):
-    if os.path.exists(path):
+    if path_exists(path):
         mtime = os.path.getmtime(path)
-    else:
-        ext, archive_file, _= _get_archive_paths(path)
-        if ext is None:
-            raise IOError, "path %s does not exists" % path
-        else:
-            mtime = os.path.getmtime(archive_file)
-        
-    return mtime
+        return mtime
+    raise IOError, "path %s does not exists" % path
         
 
 def get_handler(ext):
     return handlers.get(ext)
+
+def splitext(f):
+    filename, ext = os.path.splitext(f)
+    if ext == '.gz':
+        _, ext2 = splitext(filename)
+        if ext2 == '.tar':
+            ext = '.tar.gz'
+    
+    return filename,ext
+
+def get_paths(work_path, indexed_paths, to_index, target_path):
+    for root, _, files in os.walk(work_path):
+        for f in files:
+            _, ext = splitext(f)
+            if ext in archive_handlers:
+                archive_path = path_join(root, f)
+                relpath = os.path.relpath(archive_path, target_path)
+                if relpath not in indexed_paths or relpath in to_index:
+                     
+                    with archive_handlers[ext].extract(archive_path) as realroot:
+                        for root2, _, files2 in os.walk(realroot):
+                            for f in files2:
+                                
+                                real_path = path_join(root2, f)
+                                index_path = path_join(relpath, os.path.relpath(real_path, realroot))
+                                
+                                yield real_path, index_path, relpath
+            else:
+                real_path = path_join(root, f)
+                index_path = os.path.relpath(real_path, target_path)
+                yield real_path, index_path, index_path
 
 # https://whoosh.readthedocs.org/en/latest/indexing.html#incremental-indexing
 def incremental_index(ix, target_path, indexables, work_path):
@@ -332,9 +397,11 @@ def incremental_index(ix, target_path, indexables, work_path):
         # Loop over the stored fields in the index
         for fields in searcher.all_stored_fields():
             indexed_path = fields['path']
-            indexed_paths.add(indexed_path)
+            real_path = fields['real_path']
+
+            indexed_paths.add(os_path(real_path))
             
-            filepath = os.path.join(target_path, indexed_path)
+            filepath = path_join(target_path, real_path)
             #if not os.path.exists(filepath):
             if not path_exists(filepath):
                 # This file was deleted since it was indexed
@@ -350,33 +417,31 @@ def incremental_index(ix, target_path, indexables, work_path):
                     # The file has changed, delete it and add it to the list of
                     # files to reindex
                     writer.delete_by_term('path', indexed_path)
-                    to_index.add(indexed_path)
+                    to_index.add(os_path(real_path))
 
         count = 0
         try:
             work_path = work_path or target_path
-            for root, _, files, realroot  in walk_path(work_path):
-                for f in files:
-                    filename, ext = os.path.splitext(f)
-                    if ext in indexables:
-                        virtualpath = os.path.join(root, f)
-                        relpath = os.path.relpath(virtualpath, target_path)
-                        if relpath not in indexed_paths or relpath in to_index:
-                            logger.info("indexing... %s", relpath)
-                            hdr = get_handler(ext)
-                            if hdr is None:
-                                content = "" 
-                            else:
-                                try:
-                                    filepath = os.path.join(realroot, f)
-                                    content = hdr.extract_content(filepath.encode('utf-8'))
-                                    writer.add_document(title=filename, content=content,
-                                                path=relpath, filetype=ext, time=getmtime(virtualpath))
-                                    count+=1
-                                except KeyboardInterrupt:
-                                    raise
-                                except:
-                                    logger.exception("error occurred")
+            
+            for real_path, index_path, archive_path in get_paths(work_path, indexed_paths, to_index, target_path):
+                _,ext = splitext(real_path)
+                if ext in indexables:
+                    filename = os.path.basename(real_path)
+                    if index_path not in indexed_paths or index_path in to_index:
+                        logger.info("indexing... %s", index_path)
+                        hdr = get_handler(ext)
+                        if hdr is None:
+                            content = "" 
+                        else:
+                            try:
+                                content = hdr.extract_content(real_path.encode('utf-8'))
+                                writer.add_document(title=filename, content=content,
+                                            path=std_path(index_path), filetype=ext, time=getmtime(path_join(target_path,archive_path)), real_path=std_path(archive_path))
+                                count+=1
+                            except KeyboardInterrupt:
+                                raise
+                            except:
+                                logger.exception("error occurred")
 
         except KeyboardInterrupt:
             pass
